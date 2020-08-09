f4e60ebb876beb09e955cb77a644f3706782a40e
Nate <nathanieldiamond97@gmail.com>
2020-08-09 12:54:14 -0600

    created tables

---
+++ EmployeeSQL/data/schema.sql
@@ -0,0 +1,43 @@
+-- drop tables if they exist
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS titles;
+------------------------------------------------
+-- create tables
+
+create table departments(
+	dept_no varchar primary key,
+	dept_name varchar
+);
+create table dept_emp(
+	emp_no int,
+	dept_no varchar
+	
+);
+create table dept_manager(
+	dept_no varchar,
+	emp_no int
+);
+create table salaries(
+	emp_no int,
+	salary int
+);
+create table employees(
+	emp_no int,
+	emp_title varchar,
+	birth_date varchar,
+	first_name varchar,
+	last_name varchar,
+	sex varchar(2),
+	hire_date varchar
+);
+create table titles(
+	title_id varchar,
+	title varchar
+);
+
+select * from titles
+---------------------------------
